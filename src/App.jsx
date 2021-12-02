bf5d6483aef609e82c7420149db97c6f3784e868
Usman Khalid <usmankhalid2591@gmail.com>
2021-12-02 15:34:24 +0500

    add a state variable for students

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
+      studends = [],
       buttonClicked: "",
       assignments: [] /*Below this line, add the students state variable*/,
       grades: {}
