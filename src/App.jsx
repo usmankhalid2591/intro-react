54dfe3b02aa6ce205595b9f0da480d24edc0350b
Usman Khalid <usmankhalid2591@gmail.com>
2021-12-02 15:25:56 +0500

    uncomment assignment rendering

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ class App extends React.Component {
     let tabChoice = <div />;
 
     /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -60,7 +60,7 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below to render students*/
 
